33956af7d1627d3da04a4f59c3aa6c9d34b8231d
mohon sarker <mohonsarker40@gmail.com>
2026-04-13 16:19:53 +0600

    Soil Device & Device Logs

--- app/Models/DeviceStatus.php
+++ app/Models/DeviceStatus.php
@@ -2,13 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\ModelScopes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DeviceStatus extends Model
 {
     use HasFactory;
-    protected $fillable = ['device_id', 'online', 'temperature', 'humidity', 'battery_percentage', 'temp_alarm', 'hum_alarm', 'recorded_at'];
+    use ModelScopes;
+
+    protected $table = 'device_statuses';
+    protected $fillable = [
+        'device_id',
+        'online',
+        'temperature',
+        'humidity',
+        'battery_percentage',
+        'temp_alarm',
+        'hum_alarm',
+        'recorded_at'
+    ];
 
     public function device()
     {
