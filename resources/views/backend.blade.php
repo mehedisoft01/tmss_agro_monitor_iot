ff2a586bacd3491e418fd2fdb29f21489e9789a6
mdshihabuddin78 <shihab110897@gmail.com>
2026-04-18 13:26:06 +0600

    update

--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -20,7 +20,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="manifest" href="/manifest.json">
+    <link rel="manifest" href="{{assets('manifest.json')}}">
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 
