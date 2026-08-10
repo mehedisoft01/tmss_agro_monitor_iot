647b9532f0ab01095d057fe6beb462c9774f99f0
mdshihabuddin78 <shihab110897@gmail.com>
2026-08-10 12:03:02 +0600

    update

--- app/Console/Commands/GenerateSiteReadingsReport.php
+++ app/Console/Commands/GenerateSiteReadingsReport.php
@@ -47,7 +47,8 @@ class GenerateSiteReadingsReport extends Command
                 FROM(
                 
                     WITH RECURSIVE time_series AS (
-                        SELECT TIMESTAMP('2026-06-15 00:00:00') AS dt
+                        -- SELECT TIMESTAMP('2026-08-01 00:00:00') AS dt
+                    SELECT TIMESTAMP((select DATE_ADD(DATE(NOW()), interval -7 day))) AS dt
 
                         UNION ALL
 
@@ -92,7 +93,8 @@ class GenerateSiteReadingsReport extends Command
 
                         FROM site_readings a
 
-                        WHERE a.created_at >= '2026-06-15'
+                        -- WHERE a.created_at >= '2026-08-01'
+                        WHERE a.created_at >= (select DATE_ADD(DATE(NOW()), interval -7 day))
                         AND a.temperature > 0
                         AND a.humidity > 0
                         AND a.conductivity > 0
@@ -217,7 +219,8 @@ class GenerateSiteReadingsReport extends Command
 
                                 FROM site_readings a
 
-                                WHERE a.created_at >= '2026-06-15'
+                                -- WHERE a.created_at >= '2026-08-01'
+                                WHERE a.created_at >= (select DATE_ADD(DATE(NOW()), interval -7 day))
                                 AND a.temperature > 0
                                 AND a.humidity > 0
                                 AND a.conductivity > 0
@@ -266,18 +269,7 @@ class GenerateSiteReadingsReport extends Command
 
                 ) ff
 
-                LEFT JOIN site_readings_report bt 
-                    ON ff.site_idd = bt.site_id 
-                    AND ff.reading_time = DATE_FORMAT(
-                        DATE_SUB(
-                            bt.created_at,
-                            INTERVAL MINUTE(bt.created_at) % 15 MINUTE
-                        ),
-                        '%Y-%m-%d %H:%i:00'
-                    )
-
-                WHERE bt.id IS NULL
-                AND ff.reading_time >= ff.start_date
+                WHERE ff.reading_time >= ff.start_date
                 AND (
                     ff.close_date IS NULL
                     OR ff.reading_time <= ff.close_date
